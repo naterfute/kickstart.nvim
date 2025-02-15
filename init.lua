f6c61d1407c11e01a7e876876cc812fb010503dc
KalebSchmidlkofer <kaleb@ashbyte.com>
2025-02-14 16:53:05 -0800

    feat: moved markview to it's own plugin file

--- init.lua
+++ init.lua
@@ -870,16 +870,12 @@ require('lazy').setup({
       --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     end,
   },
-  {
-    'OXY2DEV/markview.nvim',
-    lazy = true, -- Recommended
-    -- ft = "markdown" -- If you decide to lazy-load anyway
 
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter',
-      'nvim-tree/nvim-web-devicons',
-    },
-  },
+  --   dependencies = {
+  --     'nvim-treesitter/nvim-treesitter',
+  --     'nvim-tree/nvim-web-devicons',
+  --   },
+  -- },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
